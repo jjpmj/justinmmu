f8115afa9ef380e4ead60603ed3defb65c501d73
jjpmj <pgmx84dbfw@privaterelay.appleid.com>
2025-11-20 00:52:20 +0000

    Added text input section

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 st.title("My First Streamlit App")
 st.write("This app was deployed using Streamlit Cloud!")
+
+# Text input section
+user_text = st.text_input("Type something:")
+
+# Display typed text
+if user_text:
+    st.write("You typed:", user_text)
